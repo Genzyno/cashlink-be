3136be8ee675a4a91c7329db269d04b94c1c70be
Genzyno <mygenzyno@gmail.com>
2026-05-01 13:20:17 +0530

    security config file added final

--- src/main/java/com/john/ledger/auth/config/SecurityConfig.java
+++ src/main/java/com/john/ledger/auth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.john.ledger.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,18 +14,23 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(Customizer.withDefaults())   // USES YOUR EXISTING CorsConfig
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
+
+                .httpBasic(httpBasic -> httpBasic.disable())   // REMOVE POPUP
+                .formLogin(form -> form.disable())             // REMOVE LOGIN PAGE
+
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
                         .requestMatchers(
                                 "/myledger-api/auth/**",
                                 "/myledger-api/public/**",
                                 "/myledger-api/actuator/**"
                         ).permitAll()
-                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
+
                         .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+                );
 
         return http.build();
     }

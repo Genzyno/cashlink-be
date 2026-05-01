a9c462d7519ca191ddd37f451bea989bac509ae0
Genzyno <mygenzyno@gmail.com>
2026-05-01 13:11:51 +0530

    security config file added

--- src/main/java/com/john/ledger/auth/config/SecurityConfig.java
+++ src/main/java/com/john/ledger/auth/config/SecurityConfig.java
@@ -9,22 +9,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/actuator/**",
                                 "/myledger-api/auth/**",
-                                "/myledger-api/public/**",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/myledger-api/actuator/**",
+                                "/myledger-api/v3/api-docs/**",
+                                "/myledger-api/swagger-ui/**"
                         ).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(httpBasic -> httpBasic.disable())
-                .formLogin(form -> form.disable());
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
